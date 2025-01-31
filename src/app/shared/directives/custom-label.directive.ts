5f80743461b846cbda7b38154aed83d723dc5c43
Andy Castle <masterchief_and@live.com>
2025-01-31 14:00:47 -0500

    Las directivas podrian ser bastante utilis para poner los errores de los inputs

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -6,11 +7,18 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     console.log({ color: value });
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    // console.log(value);
+    this.setErrorMessage();
   }
 
   constructor(private element: ElementRef<HTMLElement>) {
@@ -30,5 +38,42 @@ export class CustomLabelDirective implements OnInit {
     if (!this.htmlElement) return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+
+    // if (!this._errors) {
+    //   this.htmlElement.nativeElement.innerText = 'No hay errores';
+    //   return;
+    // }
+
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    // console.log(errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerText = `Se necesitan minimo ${min} caracteres de ${current}`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText =
+        'Se necesita formato de  correo electronico';
+      return;
+    }
   }
 }
