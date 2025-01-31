5ef63737e4c9d10769a699855e63ea334ff9d9ae
Andy Castle <masterchief_and@live.com>
2025-01-31 13:41:26 -0500

    Se hizo la función para poder cambiar de color el span usando la directiva

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value: string) {
+    console.log({ color: value });
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor(private element: ElementRef<HTMLElement>) {
     console.log(element);
@@ -15,6 +22,13 @@ export class CustomLabelDirective implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('Directiva - NgOnit');
+    // console.log('Directiva - NgOnit');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 }
